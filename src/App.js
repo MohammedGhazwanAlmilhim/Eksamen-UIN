2fad5ca339c8e792ef5b5283c2aa8018fbad7bd4
Mohammed Ghazwan Almilhim <52640007+MohammedGhazwanAlmilhim@users.noreply.github.com>
2023-04-25 18:58:30 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
-import GameShop from './pages/GameShop';
-import MyGames from './pages/MyGames';
-import MyFavorites from './pages/MyFavourites';
+import GameShop from './components/GameShop';
+import MyGames from './components/MyGames';
+import MyFavorites from './components/MyFavourites';
 import GamePage from './pages/GamePage';
 import Signin from './components/Signin';
 import './css/main.css';
--- src/pages/GameShop.js
+++
@@ -1,5 +0,0 @@
-//GamesShop som lister opp alle spill fra store-array inne i games.js filen
-
-//Hent ut de tre nyeste spillene for visning i dashboard. 
-
-//Hent ut de 10 nyeste for visning på /gameshop (når du klikker linken "Visit Shop" eller i menyen)
