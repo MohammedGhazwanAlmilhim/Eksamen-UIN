369b0145e716d2885d99c584199a5cbda01e3558
Mohammed Ghazwan Almilhim <52640007+MohammedGhazwanAlmilhim@users.noreply.github.com>
2023-04-27 12:12:15 +0200

    update

--- src/components/MyFavourites.js
+++ src/components/MyFavourites.js
@@ -1,17 +1,19 @@
 function MyFavourites({ favourites }) {
   return (
-    <div>
-      <h2>My Favourites:</h2>
-      {favourites && favourites.length > 0 ? (
-        <ul>
-          {favourites.map((game) => (
-            <li key={game.id}>{game.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>You have no favourites yet!</p>
-      )}
-    </div>
+    <aside>
+      <h2>My Favourites</h2>
+      <div className="my-favourites">
+        {favourites && favourites.length > 0 ? (
+          <ul>
+            {favourites.map((game) => (
+              <li key={game.id}>{game.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>You have no favourites yet!</p>
+        )}
+      </div>
+    </aside>
   );
 }
 
